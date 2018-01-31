2f79e45953cc09ba2ca470d055f685a98d40298c
Juan Pablo Flores Galindo <juanfg@outlook.com>
2018-01-31 02:00:32 -0600

    facebook profile pic

--- w3/public/js/facebook.js
+++ w3/public/js/facebook.js
@@ -65,13 +65,22 @@ window.fbAsyncInit = function () {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function (response) {
-    console.log('Successful login for: ' + response.name);
+  // FB.api('/me', function (response) {
+  //   console.log('Successful login for: ' + response.name);
 
-    // document.getElementById('status').innerHTML =
-    //   'Thanks for logging in, ' + response.name + '!';
+  //   showTabs();
+  //   document.getElementById('status').innerHTML =
+  //     'Thanks for logging in, ' + response.name + '!';
+  // });
+
+  FB.api("/me/picture?width=180&height=180",  function(response) {
+
     showTabs();
-  });
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+    $('#ProfilePicture').attr("src", response.data.url);
+  });  
+
 }
 
 function showTabs() {
